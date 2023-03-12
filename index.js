140ed20e1de28e77035e54e2e26cf9c6ef124af7
NaomiBu <naomie.b@hotmail.co.uk>
2023-03-12 19:55:27 +0100

    Added nav-bar

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const underline = document.querySelector('#underline');
+const underline = document.querySelector('#underline')
 const menu = document.querySelector('nav');
 
-menu.addEventListener( 'click', e => { 
-if (e.target.nodeName = 'A') { 
-    underline.style.left = e.target.offsetLeft + 'px';
-    underline.style.width = e.target.offsetWidth + 'px';
-  }
-});
-
+menu.addEventListener('click', e => {
+  if (e.target.nodeName ==='A') {
+  underline.style.left = e.target.offsetLeft + 'px';
+  underline.style.width = e.target.offsetWidth + 'px';
+}
+}); 
