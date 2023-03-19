0b45490a36e65ff4b43661f768565e770bc3ed74
Naomi B <115047639+NaomiBu@users.noreply.github.com>
2023-03-19 17:24:06 +0000

    Updated navigation bar

--- index.js
+++
@@ -1,9 +0,0 @@
-const underline = document.querySelector('#underline')
-const menu = document.querySelector('nav');
-
-menu.addEventListener('click', e => {
-  if (e.target.nodeName ==='A') {
-  underline.style.left = e.target.offsetLeft + 'px';
-  underline.style.width = e.target.offsetWidth + 'px';
-}
-}); 
